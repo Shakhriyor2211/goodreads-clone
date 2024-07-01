29bc1e6c22c04acb8493157545b4f07dde117a29
Shakhriyor <shakhriyormamadaliev@gmail.com>
2024-07-01 18:16:18 +0500

    initial

---
+++ goodread/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from goodread.views import landing_page
+
+urlpatterns = [
+    path('', landing_page, name='landing'),
+    path('users/', include('users.urls'), name='users'),
+
+
+    path('admin/', admin.site.urls),
+]
