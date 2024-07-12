a02d1b1a81958d3a6cbae1e503f72fa6bea3e653
Shakhriyor <shakhriyormamadaliev@gmail.com>
2024-07-12 17:36:26 +0500

    test

--- goodread/urls.py
+++ goodread/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from goodread import settings
-from goodread.views import LandingView, NotFoundView
+from goodread.views import LandingView, NotFoundView, HomeView
 
 handler404 = NotFoundView.as_view()
 
 
 urlpatterns = [
     path("", LandingView.as_view(), name="landing"),
+    path("home/", HomeView.as_view(), name="home"),
     path("users/", include("users.urls")),
     path("books/", include("books.urls")),
 
--- goodread/views.py
+++ goodread/views.py
@@ -1,6 +1,10 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
+
+from books.models import BookReview
 
 
 class LandingView(TemplateView):
@@ -8,5 +12,24 @@ class LandingView(TemplateView):
 
 
 
+class HomeView(View):
+    def get(self, request):
+
+        book_reviews = BookReview.objects.all().order_by("-created_time")
+
+        size = request.GET.get("page_size", 2)
+        paginator = Paginator(book_reviews, size)
+
+        page = request.GET.get("page", 1)
+        page_obj = paginator.get_page(page)
+
+        return render(request, "home.html", {"page_obj": page_obj})
+
+
+
+
+
 class NotFoundView(TemplateView):
     template_name = "404.html"
+
+
